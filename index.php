e4fe103350ecea382f0ede11cc2b5928064a5000
liv <igor.lytkin.2020@ya.ru>
2023-07-06 11:00:56 +0700

    Initial

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// put your PHP code here
+$myarr = array();
+$myarr[0] = readline();
+$myarr[1] = readline();
+$myarr[2] = readline();
+$myarr[3] = readline();
+$i =  readline();
+echo $myarr[$i];
+?>
